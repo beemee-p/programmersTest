dfc03a06365099549418a2b5c97d55282a4e796c
yunapark <raina.y.park@gmail.com>
2023-05-03 23:13:16 +0900

    add: replaceStrToInt

--- honorList.js
+++ honorList.js
@@ -2,6 +2,7 @@
 // k일까지 최하점을 배열에 담아준 뒤,
 // k일까지 점수를 오름차순으로 바꾸고 다음 날의 점수와 비교하며 교체해준다.
 // 최하점을 추가해준다.
+
 function solution(k, score) {
     let arr = sortScore(score.slice(0, k));
     const answer = arr.map(v => v = arr[k - 1]);
